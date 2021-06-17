b1d1565533703d1c9209a3baf53b9714ed574aba
Tuhin Subhra De <tuhinsubhrade069@gmail.com>
2021-06-17 21:41:34 +0530

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,9 +3,10 @@ echo "\
 [general]\n\
 email = \"your-email@domain.com\"\n\
 " > ~/.streamlit/credentials.toml
-echo "\
-[server]\n\
-headless = true\n\
-enableCORS=false\n\
-port = $PORT\n\
+echo "[theme]
+base="dark"
+[server]
+headless = true
+port = $PORT
+enableCORS = false
 " > ~/.streamlit/config.toml
